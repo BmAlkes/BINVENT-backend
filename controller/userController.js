0ce49441d1f2fe1037bc6513c732cdf2dec063e7
bmalkes <bmalkes@gmail.com>
2022-12-24 18:37:48 +0200

    Reset password route

--- controller/userController.js
+++ controller/userController.js
@@ -272,6 +272,36 @@ const forgotPassword = asyncHandler(async (req, res) => {
     }
 });
 
+// REset password
+const resetPassword = asyncHandler(async (req, res) => {
+    const { password } = req.body;
+    const { resetToken } = req.params;
+
+    //Hash token , then compare to token in DB
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    //Find Toke in DB
+    const userToken = await Token.findOne({
+        token: hashedToken,
+        expiresAt: { $gt: Date.now() },
+    });
+    if (!userToken) {
+        res.status(404);
+        throw new Error("Invalid or Expired Token");
+    }
+    // Find user
+
+    const user = await User.findOne({ _id: userToken.userId });
+    user.password = password;
+    await user.save();
+    res.status(200).json({
+        message: " Password Reset Sucessful, Please Login",
+    });
+});
+
 module.exports = {
     registerUser,
     loginUser,
@@ -281,4 +311,5 @@ module.exports = {
     updateUser,
     changePassword,
     forgotPassword,
+    resetPassword,
 };
